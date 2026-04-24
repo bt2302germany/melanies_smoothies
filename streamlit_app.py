82d422f84a534d02eed72d3d8b144afb06c37d31
bt2302germany <benjamin.tufo@infomotion.de>
2026-04-24 14:18:20 +0200

    Update streamlit_app.py
    cxn to cnx chage in line 23 // session

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ from snowflake.snowpark.functions import col
 #Connection tabelle  fruits
 from snowflake.snowpark.context import get_active_session
 cnx = st.connection("snowflake")
-session = cxn.session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 #multislelect
